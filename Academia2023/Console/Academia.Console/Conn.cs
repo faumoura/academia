0bae39de7c62d5c2ad8cef56c32db63d679810f5
Filipe Moura <filipe.moura@pentare.com.br>
2023-03-31 19:06:17 -0300

    adicao de console

---
+++ Academia2023/Console/Academia.Console/Conn.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Tooling.Connector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Academia.Console
+{
+    class Conn
+    {
+        public static IOrganizationService GerarService(string connectionString)
+        {
+            if (string.IsNullOrEmpty(connectionString))
+                throw new Exception(string.Format("Parametro {0} não pode ser nulo ou vazio.", nameof(connectionString)));
+            try
+            {
+                ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+                CrmServiceClient crmService = new CrmServiceClient(connectionString);
+
+                return crmService;
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        internal static IOrganizationService GerarService(object p)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Academia2023/Console/Academia.Console/Program.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using Microsoft.Xrm.Sdk.Query;
+
+namespace Academia.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Iniciando Conexão com o CRM");
+            IOrganizationService serviceOrigem = Conn.GerarService(ConfigurationManager.AppSettings["ConexaoAppSecrect"]);
+            System.Console.WriteLine("Conexão estabelecida com o CRM");
+
+            Academia academia = new Academia();
+          
+            Guid idRegistro = academia.CriarRegistro(serviceOrigem);
+            System.Console.WriteLine("Registro com ID: {0} criado", idRegistro.ToString());
+          
+            academia.AtualizarRegistro(serviceOrigem, idRegistro);
+            System.Console.WriteLine("Registro com ID: {0} atualizado", idRegistro.ToString());
+            
+            academia.DeletarRegistro(serviceOrigem, idRegistro);
+            System.Console.WriteLine("Registro com ID: {0} deletado", idRegistro.ToString());
+        }
+    }
+
+    public class Academia
+    {
+        public EntityReference RetornarCategoriaTerror(IOrganizationService service)
+        {
+            QueryExpression query = new QueryExpression();
+            query.EntityName = "academia_categoriadefilmes";
+            query.Criteria.AddCondition("academia_nome", ConditionOperator.Equal, "Terror");
+            Entity categoriaFilmes = service.RetrieveMultiple(query).Entities.FirstOrDefault();
+
+            return categoriaFilmes.ToEntityReference();
+        }
+
+        public EntityReference RetornarContato(IOrganizationService service)
+        {
+            QueryExpression query = new QueryExpression();
+            query.EntityName = "contact";
+            query.Criteria.AddCondition("emailaddress1", ConditionOperator.Equal, "someone_h@example.com");
+            Entity contato = service.RetrieveMultiple(query).Entities.FirstOrDefault();
+
+            return contato.ToEntityReference();
+        }
+
+        public Guid CriarRegistro(IOrganizationService service)
+        {
+
+            EntityReference categoria = RetornarCategoriaTerror(service);
+            EntityReference contato = RetornarContato(service);
+
+            Entity criarAluguelDeFilme = new Entity("academia_alugueldefilmes");
+            criarAluguelDeFilme["academia_haquantotempoconhecealocadora"] = new OptionSetValue(645870003); //seis meses
+            criarAluguelDeFilme["academia_nome"] = "Aluguel criado pela console";
+            criarAluguelDeFilme["academia_contato"] = contato;
+            criarAluguelDeFilme["academia_categoriadefilmes"] = categoria;
+
+            Guid idRegistro = service.Create(criarAluguelDeFilme);
+            return idRegistro;
+        }
+
+        public void AtualizarRegistro(IOrganizationService service, Guid id)
+        {
+            Entity atualizarAluguelDeFilme = new Entity("academia_alugueldefilmes", id);
+
+            atualizarAluguelDeFilme["academia_haquantotempoconhecealocadora"] = new OptionSetValue(645870004); //um ano ou mais meses
+            atualizarAluguelDeFilme["academia_nome"] = "Aluguel atualizado pela console";
+
+            service.Update(atualizarAluguelDeFilme);
+        }
+
+        public void DeletarRegistro(IOrganizationService service, Guid id)
+        {
+
+            service.Delete("academia_alugueldefilmes", id);
+        }
+    }
+
+}
